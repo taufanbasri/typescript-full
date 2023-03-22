88024c3bd49def7566a805475f816b3804cd221a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-22 12:32:10 +0700

    Tuples Type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-let sales = 123_456_789 // let sales: number = 123_456_789
-let course = 'TypeScript' // let course: string = 'TypeScript'
-let is_published = true // let is_published: boolean = true
+let user: [number, string] = [1, 'Taufan']
