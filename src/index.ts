75caadda1cbd4bc67e220deda262e5b758db1ebb
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-22 11:58:17 +0700

    Buildt-In Type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-let age: number = 30
-if (age < 50)
-  age += 10
-console.log(age)
+let sales = 123_456_789 // let sales: number = 123_456_789
+let course = 'TypeScript' // let course: string = 'TypeScript'
+let is_published = true // let is_published: boolean = true
